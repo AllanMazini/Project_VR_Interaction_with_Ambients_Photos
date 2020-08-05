8e0a6f8a607c0d2bd8fcb6bf2741eb6e4054e5e4
Allan Mazini <aln.mazini@gmail.com>
2020-08-05 11:12:16 -0300

    Start_Create first Ambient

---
+++ Assets/Scripts/ControllerCaam.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerCaam : MonoBehaviour
+{
+    public float Speed; 
+    
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.down * Speed * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.up * Speed * Time.deltaTime);
+            
+        }
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Rotate(Vector3.left * Speed * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Rotate(Vector3.right * Speed * Time.deltaTime);
+        }
+    }
+}
