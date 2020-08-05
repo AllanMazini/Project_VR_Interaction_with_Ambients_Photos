63d128b25121023eea9debcdc2260b9ff01ad359
Allan Mazini <aln.mazini@gmail.com>
2020-08-05 16:15:21 -0300

    Create_Button Continous and Move Cam

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class Button : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+     public float input;
+     public float sensitility;
+     bool pressicion;
+    
+public void OnPointerDown(PointerEventData eventData){
+    pressicion = true;
+}
+public void OnPointerUp(PointerEventData eventData){
+    pressicion = false;
+}
+
+    void Update()
+    {
+        if (pressicion)
+        {
+            input += Time.deltaTime * sensitility;
+        }else{
+            input -= Time.deltaTime * sensitility;
+        }
+        input = Mathf.Clamp(input, 0 , 1);
+    }
+}
--- Assets/Scripts/ControllerCaam.cs
+++ Assets/Scripts/ControllerCaam.cs
@@ -1,36 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 
 public class ControllerCaam : MonoBehaviour
 {
+    public GameObject _buttonRight;
+    public GameObject _buttonLeght;
+    Button _component, _component2;
     public float Speed; 
-    
-
+   
     void Start()
     {
+        _component = _buttonRight.GetComponent<Button>();
+        _component2 = _buttonLeght.GetComponent<Button>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.Rotate(Vector3.down * Speed * Time.deltaTime);
-        }
-        if(Input.GetKey(KeyCode.RightArrow))
-        {
-            transform.Rotate(Vector3.up * Speed * Time.deltaTime);
-            
-        }
-        if(Input.GetKey(KeyCode.UpArrow))
-        {
-            transform.Rotate(Vector3.left * Speed * Time.deltaTime);
-        }
-        if(Input.GetKey(KeyCode.DownArrow))
-        {
-            transform.Rotate(Vector3.right * Speed * Time.deltaTime);
-        }
+      
+        transform.Rotate(0, Speed * _component.input * Time.deltaTime, 0 );
+        transform.Rotate(0, -Speed * _component2.input * Time.deltaTime, 0 );
+
     }
+
+ 
 }
