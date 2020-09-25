095b79716658a0fe7b5482b986aa4016fcc46738
Allan Mazini <50208213+AllanMazini@users.noreply.github.com>
2020-09-25 11:41:34 -0300

    Menu
    Em construcao

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+
+    public GameObject pauseMenu;
+    private bool activePausedMenu;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            activePausedMenu = !activePausedMenu;
+            pauseMenu.SetActive(activePausedMenu);
+        }
+
+    }
+
+    public void sala()
+    {
+        SceneManager.LoadScene(1);    
+    }
+
+
+    // public void activeOption(GameObject go)
+    // {
+    //     go.SetActive(true);
+    // }
+
+    // public void exitOption(GameObject go)
+    // {
+    //     go.SetActive(false);
+    // }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+
+}
